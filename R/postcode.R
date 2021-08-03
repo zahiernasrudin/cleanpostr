eba59382ca35dd23a255d3814d148940af8ed923
Zahier Nasrudin <zahiernasrudin@gmail.com>
2021-08-03 16:34:10 +0800

    Add dependencies

--- R/postcode.R
+++ R/postcode.R
@@ -1,8 +1,18 @@
-parse_postcode <- function(.data, column1) {
+#' Extract postcode(s) from an address
+#'
+#' @param .data Input data set
+#' @param address Address variable
+#'
+#' @return A tibble with the number postcodes found (5 digits) & the extracted postcode
+#' @export
+#'
+#' @examples
+
+parse_postcode <- function(.data, address) {
 
   .data %>%
-    mutate(postcode_count = str_count({{column1}}, "\\d{5}")) %>%
-    mutate(postcode_final = map_chr(str_extract_all({{column1}}, "\\d{5}"), ~ str_c(.x, collapse=","))) %>%
-    mutate(postcode_final = word(postcode_final, -1, sep = ","))
+    dplyr::mutate(postcode_count = stringr::str_count({{address}}, "\\d{5}")) %>%
+    dplyr::mutate(postcode_final = purrr::map_chr(stringr::str_extract_all({{address}}, "\\d{5}"), ~stringr:: str_c(.x, collapse=","))) %>%
+    dplyr::mutate(postcode_final = stringr::word(postcode_final, -1, sep = ","))
 
 }
