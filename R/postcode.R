32910c8dd2c8c88bb3274d852d0beccd0dc69b8f
Zahier Nasrudin <zahiernasrudin@gmail.com>
2021-08-15 19:36:02 +0800

    Update parse_postcode function

--- R/postcode.R
+++ R/postcode.R
@@ -1,18 +1,21 @@
-#' Extract postcode(s) from an address
+#' Extract postcode from an address
+#'
+#' @description Extracts postcode (5 digits). If more than one postcode (5 digits) can be found, it will return the last postcode found.
+#'
 #'
 #' @param .data Input data set
 #' @param address Address variable
 #'
-#' @return A tibble with the number postcodes found (5 digits) & the extracted postcode
 #' @export
 #'
 #' @examples
+#' parse_postcode("5, shah alam, 40150")
 
-parse_postcode <- function(.data, address) {
 
-  .data %>%
-    dplyr::mutate(postcode_count = stringr::str_count({{address}}, "\\d{5}")) %>%
-    dplyr::mutate(postcode_final = purrr::map_chr(stringr::str_extract_all({{address}}, "\\d{5}"), ~stringr:: str_c(.x, collapse=","))) %>%
-    dplyr::mutate(postcode_final = stringr::word(postcode_final, -1, sep = ","))
+parse_postcode <- function(address) {
 
+  extract_all <- purrr::map_chr(stringr::str_extract_all({{address}}, "\\d{5}"), ~stringr:: str_c(.x, collapse=","))
+  extract_last <- stringr::word(extract_all, -1, sep = ",")
+
+  extract_last
 }
